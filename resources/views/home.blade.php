b6709a7912b147769b74be47e2e8e84141f17fe6
darman1725 <darmansaragih1701@gmail.com>
2022-07-01 11:11:40 +0700

    Memahami blade komponen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,25 @@
 @extends('layouts.app', ['title' => 'Home Page'])
 
-@section('content', 'Ini adalah halaman beranda')
+{{-- @section('content', 'Ini adalah halaman beranda') --}}
+
+@section('content')
+
+    @component('components.alert', ['title' => 'Header component'])
+     
+    @slot('title')
+    Aplikasi <strong>Buku Tamu</strong> Badan Pusat Statistik Kota Malang
+    @endslot
+
+    Ini adalah children
+
+    @slot('footer')
+    Ini adalah footer
+    @endslot
+
+    @endcomponent
+
+    <p>
+    Ini adalah halaman beranda  
+    </p>  
+
+@endsection
